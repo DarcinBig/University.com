5b999c6640ce71f628f89ebefa61ef484c9a5608
DarcinBig <biganirodarcin@gmail.com>
2025-06-14 20:11:49 +0200

    add About, Programs, and Title components; implement sticky Navbar effect and update styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import Navbar from "./components/Navbar/Navbar"
 import Hero from "./components/Hero/Hero"
+import Programs from "./components/Programs/Programs"
+import Title from "./components/Title/Title"
+import About from "./components/About/About"
 
 function App() {
 
@@ -7,6 +10,14 @@ function App() {
     <div>
       <Navbar />
       <Hero />
+      <div className="container">
+        <Title
+          subTitle='Our Program'
+          title='What We Offer'
+        />
+        <Programs />
+        <About />
+      </div>
     </div>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/images/logo.png'
 
 const Navbar = () => {
+
+  const [sticky, setSticky] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      window.scrollY > 50 ? setSticky(true) : setSticky(false)
+    })
+  }, [])
+
   return (
-    <nav className='container'>
+    <nav className={`container ${sticky ? 'dark-nav' : '' }`}>
         <img className='logo' src={logo} alt="logo image" />
         <ul>
             <li>Home</li>
